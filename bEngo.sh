c51effbd54b6d4a17ab1846bd8be8797fc3a2ca6
Emilio Berti <squirry@emian.emian>
2020-03-26 19:20:11 +0100

    bEngo

--- bEngo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -s https://en.wikipedia.org/wiki/$1 -o tmp_wiki.html #download wikipage
-
-name=$(cat tmp_wiki.html  | grep -i '<title>' | cut -d '>' -f 2 | cut -d '<' -f 1 | cut -d '-' -f 1) #get title of the page
-
-echo $1, $name
-
-rm tmp_wiki.html
