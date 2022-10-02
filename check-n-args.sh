6cea86fb0e9da2198c17b308b552e9bf090a63ca
Emilio Berti <emilio.berti90@gmail.com>
2022-10-02 11:42:47 +0200

    Create check-n-args.sh

---
+++ check-n-args.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if(($# < 3))
+then
+  printf "%b""Error. Not enough arguments.\n"
+  printf "%b""usage: myscript file1 op file2\n"
+  exit 1
+elif (($# > 3))
+then
+  printf "%b""Error. Too many arguments.\n"
+  printf "%b""usage: myscript file1 op file2\n"
+  exit 2
+else
+  printf "%b""Argument count correct. Proceeding...\n"
+fi
